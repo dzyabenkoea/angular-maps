9a65122573946fddb3ef6c1f4e3cbed52f366f3b
Дзябенко Егор <e.dzyabenko@makeroi.ru>
2023-02-10 14:32:26 +0500

    Сделана отрисовка карты и селектора

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-maps';
+  map!:L.Map;
+
+  ngOnInit(){
+    console.log(document.querySelector('#map'))
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
 }
